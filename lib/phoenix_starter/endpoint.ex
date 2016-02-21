6a9b118b1964cfd6dd74303401cb14428d54570a
Michael Cloutier <viotech@comcast.net>
2016-02-21 16:36:36 -0500

    Turn on Gzip compression in production. 60% reduction? yes please

--- lib/phoenix_starter/endpoint.ex
+++ lib/phoenix_starter/endpoint.ex
@@ -8,7 +8,7 @@ defmodule PhoenixStarter.Endpoint do
   # You should set gzip to true if you are running phoenix.digest
   # when deploying your static files in production.
   plug Plug.Static,
-    at: "/", from: :phoenix_starter, gzip: false,
+    at: "/", from: :phoenix_starter, gzip: true,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
